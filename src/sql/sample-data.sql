958848f69a7848b4557616d5737bf594efd95d36
woynert <woynertgamer@gmail.com>
2022-10-31 13:50:48 -0500

    SQL update sales_new from PROCEDURE to FUNCTION

--- src/sql/sample-data.sql
+++ src/sql/sample-data.sql
@@ -6,14 +6,16 @@ DECLARE
     id_sale INT;
 BEGIN
 
-    CALL sales_new('asd12-5406-4506das', id_sale);
-    CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 1050 );
-    CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 50, 1000 );
-    CALL sales_add_item (id_sale, '12155-5406-4506das', 5, 20, 1000 );
+    SELECT * INTO id_sale
+	FROM funtest('asd12-5406-4506das');
+	CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 1050 );
+	CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 50, 1000 );
+	CALL sales_add_item (id_sale, '12155-5406-4506das', 5, 20, 1000 );
 
-    CALL sales_new('abc12-5406-4506das', id_sale);
-    CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 1000 );
-    CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 20, 1000 );
+    SELECT * INTO id_sale
+	FROM funtest('asd12-5406-4506das');
+	CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 1000 );
+	CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 20, 1000 );
     
 END;
 $$;
