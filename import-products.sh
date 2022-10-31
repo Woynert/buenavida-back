70f108d3c2beeeee69cc61804c8c17f4dfd3cb40
woynert <woynertgamer@gmail.com>
2022-10-31 07:40:32 -0500

    Better import data script

--- import-products.sh
+++ import-products.sh
@@ -6,4 +6,20 @@ sudo rm -r ./data/postgres/
 mkdir ./data/mongodb
 mkdir ./data/postgres
 
-docker-compose -f ./util/import-products.yml up
+docker-compose -f ./util/import-products.yml up \
+	--abort-on-container-exit \
+	--exit-code-from mongo-script
+
+if [ $? -eq 0 ]; then
+	printf "\
+===========================\n\
+Finished Successfully\n\
+===========================\n"
+else
+	printf "\
+===========================\n\
+Finished with error\n\
+===========================\n"
+fi
+
+
