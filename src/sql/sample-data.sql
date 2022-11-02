e0a4f81e7e3ee22d114f120e8556173cee038439
woynert <woynertgamer@gmail.com>
2022-11-02 05:24:38 -0500

    Update FLOAT to NUMERIC type

--- src/sql/sample-data.sql
+++ src/sql/sample-data.sql
@@ -8,14 +8,14 @@ BEGIN
 
     SELECT * INTO id_sale
 	FROM sales_new('asd12-5406-4506das');
-	CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 1050 );
-	CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 50, 1000 );
-	CALL sales_add_item (id_sale, '12155-5406-4506das', 5, 20, 1000 );
+	CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 50.4 );
+	CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 50, 10.3 );
+	CALL sales_add_item (id_sale, '12155-5406-4506das', 5, 20, 15 );
 
     SELECT * INTO id_sale
 	FROM sales_new('asd12-5406-4506das');
-	CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 1000 );
-	CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 20, 1000 );
+	CALL sales_add_item (id_sale, '10155-5406-4506das', 2, 0, 20.5);
+	CALL sales_add_item (id_sale, '11155-5406-4506das', 1, 20, 5.5);
     
 END;
 $$;
